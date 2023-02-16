17df215e0b49dfdc483a97dd97975c9a8f5eec1f
Shashank Khatri <khatri.shashank@outlook.com>
2023-02-16 18:53:49 +0530

    remove discord URL

--- hookdeck.js
+++ hookdeck.js
@@ -7,7 +7,7 @@ exports.handler = async (event, context) => {
     const username = body.sender.login;
     const avatarUrl = body.sender.avatar_url;
     const repoName = body.repository.name;
-    const res = await axios.post('https://discord.com/api/webhooks/1065405532464238742/nrYJf0bneeJAt1gx1O7LMQME41hn67CEL6JSzfyRUsJ8k6gO_YzzL7BZ67nSF8bwFFdf', {
+    const res = await axios.post('', {
       content: ` ${username} just starred ${repoName}! `,
       embeds: [
         {
